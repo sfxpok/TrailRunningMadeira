cff681aeac99c3c2bf87db20a61c0ecb057f3706
sfxpok <14007768+sfxpok@users.noreply.github.com>
2021-01-09 21:49:14 +0100

    Initial commit.

---
+++ TrailRunningMadeira/TrailRunningMadeira/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TrailRunningMadeira.ViewModels;
+using Xamarin.Forms;
+
+namespace TrailRunningMadeira.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ TrailRunningMadeira/TrailRunningMadeira/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TrailRunningMadeira.Models;
+using TrailRunningMadeira.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrailRunningMadeira.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
